720379aca9b13c2088d0ae11581ac44b965c2336
OONQT <r0blox1053@outlook.com>
2021-04-14 16:42:02 -0700

    First commit

---
+++ project-3-1-master/build/chalk/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Project 3.1",
+  .company = "C Programming With Pebble OS",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_PLATFORM_CHALK,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x55, 0x01, 0xFA, 0x50, 0x41, 0x96, 0x43, 0x13, 0x84, 0xFF, 0xD0, 0xB9, 0xF4, 0x80, 0x03, 0x47 },
+  .virtual_size = 0xb6b6
+};
